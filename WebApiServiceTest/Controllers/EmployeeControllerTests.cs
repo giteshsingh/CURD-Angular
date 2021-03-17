50ff7085aa218af5f48e9266d85bfe0f43cfd935
Gitesh <giteshsingh17@gmail.com>
2021-03-17 13:19:15 +0530

    Added unit test and tried to implement odata

---
+++ WebApiServiceTest/Controllers/EmployeeControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using WebApiService.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApiService.Common;
+using WebApiService.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Constroller.Tests
+{
+    [TestFixture()]
+    public class EmployeeControllerTests
+    {
+        private readonly IEmployeeData employee;
+
+        EmployeeControllerTests(IEmployeeData emp)
+        {
+            this.employee = emp;
+        }
+
+        [Test()]
+        public void DeleteEmployeeTest()
+        {
+
+
+            //Arrange  
+            var controller = new EmployeesController(employee);
+            //var postId = 2;
+
+            //Act  
+            var data =  controller.DeleteEmployee(Guid.NewGuid());
+
+            //Assert  
+          //  Assert.AreEqual(OkResult,data.ExecuteResultAsync().Status>(data);
+
+
+        }
+    }
+}
